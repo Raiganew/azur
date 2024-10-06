458ad7d8b10ebdf915c3954d9b2ccb25436a5239
fabian <raiganew72@gmail.com>
2024-10-06 11:20:24 -0500

    Commit inicial

---
+++ src/main/java/com/rga/main/AzurGestorApplication.java
@@ -0,0 +1,13 @@
+package com.rga.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rga/main/AzurGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rga.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
